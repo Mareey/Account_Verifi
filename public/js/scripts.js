ba22d0110ae9462be3714f10fbca93fbd2ea5b41
Mareeya <mariaadigun@gmail.com>
2019-12-02 14:00:30 +0100

    deploy correction

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,23 +1,24 @@
 //jshint esversion:6
+
+let errorMessage = document.getElementById('errorMessage');
+
+function validateForm() {
+  let userName = "Maria";
+  let passwordValue =1;
+  let name = document.forms.login.name.value;
+  let password = document.forms.login.password.value;
+
+  if (name == userName && password == passwordValue) {
+    return true;
+  }
+  else{
+    errorMessage.innerHTML ="Invalid login details";
+    return false;
+  }
+}
+
 $(document).ready(function () {
   $('select').formSelect();
   var instance = M.FormSelect.getInstance();
   instance.getSelectedValues();
 });
-let name = document.getElementById('name');
-let password = document.getElementById('password');
-
-let submitBtn = document.getElementById('submit');
-
-let errorMessage = document.getElementById('errorMessage');
-
-submitBtn.addEventListener('click', () => {
-  if (name.value !== "Maria" && password.value !== 1) {
-    errorMessage.innerHTML = 'Invalid login details';
-    event.preventDefault();
-    return false;
-  } else {
-    return true;
-  }
-});
-
